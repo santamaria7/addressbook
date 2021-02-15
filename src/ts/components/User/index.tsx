99c4370679a88393abb1412a5cc9eadefbb4a071
Marzieh Bahri <marzieh@pragmateam.dev>
2021-02-15 15:17:08 -0700

    img and name of the user added

--- src/ts/components/User/index.tsx
+++ src/ts/components/User/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
-const UserItem: React.FC<UserProps> = ({user}) => {
-    return <div>
-        User
+const UserItem: React.FC<UserProps> = ({ user }) => {
+  const showDetails = () => {
+    // TODO: open details modal
+    console.log(user);
+  };
+
+  return (
+    <div className="user" onClick={showDetails}>
+      <div className="user__img">
+        <img
+          src={user.picture.thumbnail}
+          alt={`${user.name.first} ${user.name.last}`}
+        />
+      </div>
+      <span className="user__name">{`${user.name.first} ${user.name.last}`}</span>
     </div>
-}
+  );
+};
 
 export default UserItem;
