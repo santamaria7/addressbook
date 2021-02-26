1b7443ce06455335cd1cb83091df4f6134f3e2c8
Marzieh Bahri <marzieh@pragmateam.dev>
2021-02-25 18:44:57 -0700

    added test file for saga function

---
+++ src/ts/store/saga/getusersSaga.test.js
@@ -0,0 +1,15 @@
+import {takeLatest} from "@redux-saga/core/effects";
+import {getUsers, getUsersWatcher} from "./getUsersSaga";
+
+describe('Get Users', () => {
+    const genObject = getUsersWatcher();
+
+    it('should wait for latest get_users action and call getUsers', () => {
+        expect(genObject.next().value)
+            .toEqual(takeLatest('get_users', getUsers));
+    });
+
+    it('should be done on next iteration', () => {
+        expect(genObject.next().done).toBeTruthy();
+    });
+});
